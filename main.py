744a8ac39d6b982bcf1360ef110dd824a3d8832c
Gianni Gambetti <gianni@gambetti.ca>
2023-11-07 09:12:57 -0500

    services/gandi-live-dns: support personal access tokens for auth

--- main.py
+++ main.py
@@ -12,8 +12,11 @@ def headers():
   h = {
     'Connection': None,
     'Content-Type': 'application/json',
-    'Authorization': 'Apikey ' + config.api_key
   }
+  if config.api_key:
+    h['Authorization'] = 'Apikey ' + config.api_key
+  if config.access_token
+    h['Authorization'] = 'Bearer ' + config.access_token
   return h
 
 
